67ee11ad0126f9810e347e7a22b4df37f1aa15f1
Сергей Нефедов <NefedovSA@cds.spb.ru>
2023-02-27 11:01:38 +0300

    Добавьте файлы проекта.

---
+++ JoinFittingsCommand.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FittingsJoiner
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class JoinFittingsCommand : IExternalCommand
+    {
+        static AddInId addInId = new AddInId(new Guid("CA46BA90-C633-4BA3-9B37-C1481FEC8E3A"));
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //View view = new View();
+            //ViewModel viewModel = new ViewModel(commandData);
+            Model.commandData = commandData;
+            //view.DataContext = viewModel;
+            //viewModel.CloseRequest += (s, e) => view.Close();
+            //viewModel.HideRequest += (s, e) => view.Hide();
+            //viewModel.ShowRequest += (s, e) => view.ShowDialog();
+            //view.ShowDialog();
+            Model.JoinFittings();
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,150 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FittingsJoiner
+{
+    public static class Model
+    {        
+        public static ExternalCommandData commandData { get; set; }
+        private static UIApplication uiapp { get => commandData.Application; }
+        private static UIDocument uidoc { get => uiapp.ActiveUIDocument; }
+        private static Document doc { get => uidoc.Document; }
+
+        private static Element elementToMove { get; set; }
+        private static Element elementToJoin { get; set; }
+
+        public static void JoinFittings()
+        {
+            using (TransactionGroup trg = new TransactionGroup(doc, "Соединение фитингов"))
+            {
+                trg.Start();
+                try
+                {
+                    while (true)
+                    {
+                        Connector[] connectors = FindNearestConnectors();
+                        if (connectors[0] == null)
+                        {
+                            TaskDialog.Show("Ошибка", "Первый элемент должен иметь хотя бы один открытый соединитель");
+                        }
+                        if (connectors[1] == null)
+                        {
+                            TaskDialog.Show("Ошибка", "второй элемент должен иметь хотя бы один открытый соединитель");
+                        }
+                        //TaskDialog.Show("1", connectors[0].Origin.ToString() + Environment.NewLine + connectors[1].Origin.ToString());
+                        XYZ translation = connectors[1].Origin - connectors[0].Origin;
+                        XYZ forRotation = new XYZ(0,0,1);
+                        double angle = Math.PI - CalculateAngle(connectors);
+                        //TaskDialog.Show("1", (angle * 180 / Math.PI).ToString());
+                        Line line = Line.CreateBound(connectors[0].Origin+ new XYZ(0, 0, 1), connectors[0].Origin); // + new XYZ(0, 0, 1)
+
+                        using (Transaction t = new Transaction(doc, "Соединение фитингов"))
+                        {
+                            t.Start();
+                            ElementTransformUtils.RotateElement(doc, elementToMove.Id, line, angle);
+                            XYZ pointToOrient = connectors[1].CoordinateSystem.BasisZ;
+                            XYZ pointToMove = connectors[0].CoordinateSystem.BasisZ;
+                            if ((pointToOrient + pointToMove).IsZeroLength() == false)
+                            {
+                                //angle = Math.PI +2* CalculateAngle(connectors);
+                                ElementTransformUtils.RotateElement(doc, elementToMove.Id, line, -2*angle);
+                            }
+
+                            ElementTransformUtils.MoveElement(doc, elementToMove.Id, translation);
+                            connectors[0].ConnectTo(connectors[1]);
+                            t.Commit();
+                        }
+                    }
+                }
+                catch (Exception )
+                {
+                    trg.Assimilate();
+                }
+            }
+        }
+        private static Connector[] FindNearestConnectors()
+        {
+            Connector[] connectors = new Connector[2];
+            Reference fittingReference = uidoc.Selection.PickObject(ObjectType.Element, "Выберите присоединяемый элемент (который будет перемещён");
+            Reference fittingReferenceToJoin = uidoc.Selection.PickObject(ObjectType.Element, "Выберите элемент, к которому будет выполнено присоединение");
+            elementToMove = doc.GetElement(fittingReference);
+            elementToJoin = doc.GetElement(fittingReferenceToJoin);
+            Connector[] connectorsOfFirst = FindUnusedConnectors(elementToMove);
+            Connector[] connectorsOfSecond = FindUnusedConnectors(elementToJoin);
+            XYZ clickPoint1 = fittingReference.GlobalPoint;
+            XYZ clickPoint2 = fittingReferenceToJoin.GlobalPoint;
+            double distance = 20;
+            foreach (Connector c in connectorsOfFirst)
+            {
+                if (distance > c.Origin.DistanceTo(clickPoint1))
+                {
+                    distance = c.Origin.DistanceTo(clickPoint1);
+                    connectors[0] = c;
+                }
+            }
+            distance = 20;
+            foreach (Connector c in connectorsOfSecond)
+            {
+                if (distance > c.Origin.DistanceTo(clickPoint2))
+                {
+                    distance = c.Origin.DistanceTo(clickPoint2);
+                    connectors[1] = c;
+                }
+            }
+            return connectors;
+        }
+        private static Connector[] FindUnusedConnectors(Element element)
+        {
+            Connector[] connectors = new Connector[0];
+            if (element is FamilyInstance familyInstance)
+            {
+                if (familyInstance.MEPModel != null)
+                {
+                    var ConSet = familyInstance.MEPModel.ConnectorManager.UnusedConnectors;
+                    int n = ConSet.Size;
+                    int i = 0;
+                    connectors = new Connector[n];
+                    foreach (Connector connector in ConSet)
+                    {
+                        if (connector.ConnectorType != ConnectorType.Invalid || connector.ConnectorType != ConnectorType.Logical)
+                        {
+                            connectors[i] = connector;
+                        }
+                        else
+                        {
+                            n--;
+                        }
+                        i++;
+                    }
+                    Array.Resize(ref connectors, n);
+                }                
+            }            
+            return connectors;
+        }
+        private static double CalculateAngle(Connector[] connectors)
+        {
+            double firstRotation = (elementToMove.Location as LocationPoint).Rotation;
+            double secondRotation = (elementToJoin.Location as LocationPoint).Rotation;
+
+            Connector c1 = connectors[0];
+            Connector c2 = connectors[1];
+            
+            XYZ pointToOrient = connectors[1].CoordinateSystem.BasisZ;
+            XYZ pointToMove = connectors[0].CoordinateSystem.BasisZ;            
+            
+            //Transform t = new Transform(connectors[0].CoordinateSystem);
+            //Line line = Line.CreateBound(connectors[0].Origin + new XYZ(0, 0, 1), connectors[0].Origin);
+            //t.Crea
+            
+            double result = pointToOrient.AngleTo(pointToMove);
+            return result;
+        }
+        
+    }
+}
